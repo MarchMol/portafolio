a27638c8f9f68db7b2f4ddaec514bfcb1f6429f3
MarchMol <marchmol98@gmail.com>
2024-05-31 23:30:07 -0600

    feat: contact page

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import './Contact.css'
+import im1 from '@assets/cont/github.png'
+import im2 from '@assets/cont/linked.png'
+import im3 from '@assets/cont/mail.png'
 
-const Contact = () =>{
-    return(
-        <>MY CONTACT</>
+const Contact = () => {
+    return (
+        <div className="contact">
+            <h1>Find me in:</h1>
+            <div className="contactLinks">
+               <a href="https://github.com/MarchMol">
+               <img src={im1} alt="" />
+               </a>
+               <a href="https://www.linkedin.com/in/marchmol/">
+               <img src={im2} alt="" />
+               </a>
+            </div>
+        </div>
     )
 }
 
